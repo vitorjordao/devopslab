1eccbca9754f19a285798cdc97ef731288a4b78b
Vitor Jordão <srvitorlol@gmail.com>
2022-05-22 12:23:03 -0300

    Bug Added

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "O Vitor esteve aqui!"
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.") 
+
 if __name__ == '__main__':
     app.run()
