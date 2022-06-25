43962ad62c391f5e1d249754cc9709a543aed30c
Vitor Jordão <srvitorlol@gmail.com>
2022-06-25 17:08:12 -0300

    Menssagem alterada

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "O Vitor esteve aqui!"
+    return "Hello world!"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
